1a06c14bc269572fd35bec6497ec70833973cd6d
Vladislav Kalugin <kalugin.vladislav@huawei.com>
2023-04-10 19:45:27 +0300

    initial commit 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <fstream>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+class MakeFileTarget {
+private:
+    std::string target;
+    std::string dependency;
+    std::string command;
+    std::string directory
+public:
+    MakeFileWriter() {
+
+    }
+};
 
 int main() {
+    std::ifstream cc("compile_commands.json");
+    std::ifstream lc("compile_commands.json");
+    json cc_data = json::parse(cc);
+    json lc_data = json::parse(lc);
+
+
 
     return 0;
 }
